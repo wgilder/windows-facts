7cbfd7d9ef0f7c97eb2b205f52407081f1bad18d
Walter Gildersleeve <walter@gildersleeve.eu>
2018-05-14 17:35:32 +0200

    added quotes

--- facts.d/hotfixes.ps1
+++ facts.d/hotfixes.ps1
@@ -1,2 +1,2 @@
 $_hotfixes=Get-HotFix | select Description, HotFixID | group Description | Select Name, Count, Group | ConvertTo-Json
-Write-Host "'windows-hotfixes'='$_hotfixes'"
+Write-Host "'windows-hotfixes=$_hotfixes'"
